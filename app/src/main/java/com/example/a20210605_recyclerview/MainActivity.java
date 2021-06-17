0e8f6d22d25fe68e2ed3ed79324e90e98c184a46
freshws <freshws86@gmail.com>
2021-06-17 22:38:55 +0900

    RecyclerView 작업16 (아이템을 클릭 전에도 아이템 리스트가 디폴트로 나오도록 조정, 전체 리스트가 나오는 것과 차이)

--- app/src/main/java/com/example/a20210605_recyclerview/MainActivity.java
+++ app/src/main/java/com/example/a20210605_recyclerview/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a20210605_recyclerview;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,6 +11,9 @@ import com.example.a20210605_recyclerview.service.PokemonService;
 import com.example.a20210605_recyclerview.ui.DetailActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PokemonService pokemonService;
+    private RecyclerViewPokemonAdapter recyclerViewPokemonAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +24,10 @@ public class MainActivity extends AppCompatActivity {
 
         final RecyclerView recyclerViewPokemon = findViewById(R.id.activity_main__recyclerViewPokemon);
 
-        final PokemonService pokemonService = new PokemonService();
+        pokemonService = new PokemonService();
 
         //데이터와 어뎁터가 연결되지 않은, 빈껍데이 리싸이클러 뷰 어댑터 생성
-        final RecyclerViewPokemonAdapter recyclerViewPokemonAdapter = new RecyclerViewPokemonAdapter();
+        recyclerViewPokemonAdapter = new RecyclerViewPokemonAdapter();
 
         //메인 액티비티와 어댑터 연결
         recyclerViewPokemon.setAdapter(recyclerViewPokemonAdapter);
@@ -49,23 +53,38 @@ public class MainActivity extends AppCompatActivity {
             //버튼이 눌리지 않도록 하는 것
             v.setEnabled(false);
 
-            //더 보기 버튼을 누를 때 마다 리스트를 계속해서 가져오는 코드
-            //처음에는 가져온 데이터가 없으니 recyclerViewPokemonAdapter.getDataSize() 가 0 이지만 두번째 더 보기를 누룬 후엔
-            //recyclerViewPokemonAdapter.getDataSize() 에 의해서 가져온 것 만큼 뒤의 데이터가 와야 한다.
-            //결극 recyclerViewPokemonAdapter.getDataSize(), recyclerViewPokemonAdapter.getDataSize() 메소드는 getPokemons 메소드의 매개변수 이다.
-            pokemonService.getPokemons(recyclerViewPokemonAdapter.getDataSize(), recyclerViewPokemonAdapter.getLoadCount(), responseBody -> {
+            //"더 보기" 버튼을 클릭 했을 때 아이템 리스트가 나옴
+            loadMore(v);
 
-                recyclerViewPokemonAdapter.addPokemons(responseBody.getResults());
-                //버튼이 눌려지도록 활성화
-                //v.setEnabled(true); 메소드는 getPokemons 안에 있어야함
-                //데이터가 전부 로딩되야 즉, 비동기 데이터 처리가 완료되면 버튼이 활성화됨.
-                v.setEnabled(true);
-            });
-            //v.setEnabled(true); 메소드가 여기에 있으면 아무의미 없음
-            //비동기 통신이기 때문에 위의 getPokemons가 호출되고 데이터가 오기 전에 v.setEnabled(true);가 호출된다.
-            //즉 더블클릭 하면 v.setEnabled();가 없을 때 처럼 데이터가 오기 전에 v.setEnabled(true);가 되어 더블클릭으로 인한 마구잡이 로딩이 됨
+
         });
 
+        //"더 보기" 버튼을 클릭하지 않아도 기본적으로 아이템 리스트가 디폴트로 떠 있게 해줌
+        loadMore(null);
+
+    }
+
+    public void loadMore(View view) {
+        //더 보기 버튼을 누를 때 마다 리스트를 계속해서 가져오는 코드
+        //처음에는 가져온 데이터가 없으니 recyclerViewPokemonAdapter.getDataSize() 가 0 이지만 두번째 더 보기를 누룬 후엔
+        //recyclerViewPokemonAdapter.getDataSize() 에 의해서 가져온 것 만큼 뒤의 데이터가 와야 한다.
+        //결극 recyclerViewPokemonAdapter.getDataSize(), recyclerViewPokemonAdapter.getDataSize() 메소드는 getPokemons 메소드의 매개변수 이다.
+        pokemonService.getPokemons(recyclerViewPokemonAdapter.getDataSize(), recyclerViewPokemonAdapter.getLoadCount(), responseBody -> {
+
+            recyclerViewPokemonAdapter.addPokemons(responseBody.getResults());
+            //버튼이 눌려지도록 활성화
+            //v.setEnabled(true); 메소드는 getPokemons 안에 있어야함
+            //데이터가 전부 로딩되야 즉, 비동기 데이터 처리가 완료되면 버튼이 활성화됨.
+
+            if (view != null) {
+
+                view.setEnabled(true);
+                //v.setEnabled(true); 메소드가 여기에 있으면 아무의미 없음
+                //비동기 통신이기 때문에 위의 getPokemons가 호출되고 데이터가 오기 전에 v.setEnabled(true);가 호출된다.
+                //즉 더블클릭 하면 v.setEnabled();가 없을 때 처럼 데이터가 오기 전에 v.setEnabled(true);가 되어 더블클릭으로 인한 마구잡이 로딩이 됨
+
+            }
+        });
 
     }
 }
